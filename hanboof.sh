d1cbe3119a564d8958f8cedae9e246f09a2316a1
awelvor <philippe.regent@gmail.com>
2025-11-12 20:43:08 +0100

    Create hanboof.sh

---
+++ hanboof.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+echo "handbook"
+mkdir ocr1
+git init
+echo "README'> README
+echo "test.rb" > test.rb
+echo "LICENSE" > LICENSE
+git add README test.rb LICENSE
+git commit -m 'Initial commit'
+git status
